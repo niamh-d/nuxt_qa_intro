c88cc29a77c55c11c0f748b06965ff7638320ba3
Niamh <niamh.linguist@gmail.com>
2024-12-13 23:01:26 +0200

    second unit test

--- components/AppNumber.test.ts
+++ components/AppNumber.test.ts
@@ -1,10 +1,19 @@
-import { mountSuspended } from "@nuxt/test-utils/runtime";
+import { mockNuxtImport, mountSuspended } from "@nuxt/test-utils/runtime";
 import { it, expect, describe } from "vitest";
 import AppNumber from "./AppNumber.vue";
+
+mockNuxtImport("useState", () => {
+  return () => 3;
+});
 
 describe("AppNumber", () => {
   it("can mount the component", async () => {
     const component = await mountSuspended(AppNumber);
     expect(component.html()).toContain("Number:");
+  });
+
+  it("returns the state", async () => {
+    const component = await mountSuspended(AppNumber);
+    expect(component.text()).toContain("3");
   });
 });
