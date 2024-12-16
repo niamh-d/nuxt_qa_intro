b8cb30d6615ec79ca65974e03f0b486c124a04f5
Niamh <niamh.linguist@gmail.com>
2024-12-16 16:14:13 +0200

    first pw test passing

--- e2e/app.test.ts
+++ e2e/app.test.ts
@@ -1,4 +1,4 @@
-import { setup, $fetch } from "@nuxt/test-utils/e2e";
+import { setup, $fetch, createPage, url } from "@nuxt/test-utils/e2e";
 import { describe, test, expect } from "vitest";
 
 describe("app", async () => {
@@ -7,5 +7,13 @@ describe("app", async () => {
   test("contains number as string", async () => {
     const html = await $fetch("/");
     expect(html).toContain("Number:");
+  });
+
+  test("with playwirght", async () => {
+    const page = await createPage();
+    await page.goto(url("/"), { waitUntil: "hydration" });
+    const text = await page.textContent("span");
+    const num = Number(text);
+    expect(num).toBeGreaterThan(0);
   });
 });
